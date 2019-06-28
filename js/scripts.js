2975b0ea8e0ae25c9f783ac3365f4eab5bfed96e
Stewart ilondanga <stilondanga@gmail.com>
2019-06-29 00:30:41 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,63 @@
+Vue.component('overlay-form', {
+  props:{},
+  data:function(){
+    return {
+      formInputs: [
+        {
+          'label': 'What is your full name?',
+          'name': 'fullName',
+          'value':[],
+          'prefix':"My name is",
+          'suffix':'and im so cool'
+        },
+        {
+          'label': 'What is your Email Address?',
+          'name': 'email',
+          'value':[],
+          'prefix':"You can contact me on",
+          'suffix':'or not, its cool'
+        },
+        {
+          'label': 'Which socials are you on?',
+          'name': 'socials',
+          'value':[],
+          'type': 'checkbox',
+          'prefix':"I use the following socials",
+          'suffix':'',
+          'options':[
+            {'label': 'Twitter','name':'Twitter'},
+            {'label': 'Facebook','name':'Facebook'}
+          ]
+        },
+        {
+          'label': 'What do you think facebook?',
+          'name': 'facebook',
+          'value':false,
+          'type': 'radio',
+          'prefix':"I ",'suffix':'facebook!',
+          'options':[
+            {'label': 'Love','name':'Love'},
+            {'label': 'Hate','name':'Hate'}
+          ]
+        },
+
+      ],
+      activeQuestion: 'fullName',
+      overlayActive:false,
+    }
+  },
+  methods:{
+    formSubmit: function(){
+      alert('Form Submitted');
+    }
+  },
+})
+
+var app = new Vue({
+  el: '#app',
+});
+
+
 var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
